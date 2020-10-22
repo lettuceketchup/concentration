1332029c0bf985105ceb80ee3fa8deb50e4d9522
lettuceketchup <rishishwarmanu@gmail.com>
2020-10-22 21:56:47 +0530

    Started project

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import '../../images/DeckCards.svg';
+
+const StyledCard = styled.div`
+    width: 7rem;
+    height: 10rem;
+    background-color: crimson;
+`
+
+const Card = () => {
+    return (
+        <StyledCard>
+            
+        </StyledCard>
+    );
+}
+
+export default Card;
